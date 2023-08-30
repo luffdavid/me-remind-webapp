cfab22cd99fce3e24eea390c5cc17bd0d85a28e6
luffdavid <luffd@mediamarktsaturn.de>
2023-08-30 11:27:15 +0200

    popup style changed

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -5,13 +5,20 @@ import { api_base } from '../services/constants/Constants';
 import { getTodos, completeTodo, addNewTodo, deleteTodo } from '../services/requests/TodoRequests'; 
 import Icons from '../components/icons/MuiIcons';
 import { StartCards } from '../components/cards/StartCards';
+import { Button, Input } from '@mui/material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { ModalStyle } from '../styles/ModalStyle';
 
 const Start = () => {
     const [todos, setTodos] = useState<TodoInterface[]>([]);
-	const [popupActive, setPopupActive] = useState(false);
 	const [newTodo, setNewTodo] = useState("");
 	const [dueDate, setDueDate] = useState("");
 	const [description, setDescription] = useState("");
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
 	useEffect(() => {
         loadTodos();
@@ -26,7 +33,6 @@ const Start = () => {
         }
       };
       
-	
 	const completeTodo = async (id:string) => {
 		const data = await fetch(api_base + '/todo/complete/' + id).then(res => res.json());
 
@@ -34,7 +40,6 @@ const Start = () => {
 			if (todo._id === data._id) {
 				todo.complete = data.complete;
 			}
-
 			return todo;
 		}));
 	}
@@ -44,8 +49,7 @@ const Start = () => {
           const newTodoData = await addNewTodo(newTodo, dueDate, description);
     
           setTodos([...todos, newTodoData]);
-    
-          setPopupActive(false);
+		  handleClose();
           setNewTodo('');
         } catch (error) {
           console.error('Error:', error);
@@ -60,10 +64,11 @@ const Start = () => {
           console.error('Error:', error);
         }
       };
+	
 
   return (
     <div>
-        <h1>Welcome $username</h1>
+        <h1>Welcome, $username</h1>
 		<StartCards />
 			<h4>Your Reminders</h4>
 
@@ -87,21 +92,58 @@ const Start = () => {
 				)}
 			</div>
 
-			<div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
-
-			{popupActive ? (
-				<div className="popup">
-					<div className="closePopup" onClick={() => setPopupActive(false)}>X</div>
-					<div className="content">
-						<h3>Add Task</h3>
-						<input type="text" className="add-todo-input" onChange={e => setNewTodo(e.target.value)} value={newTodo} />
-						<input type="date" className="add-todo-input-dueDate" onChange={e => setDueDate(e.target.value)} value={dueDate} />
-						<input type="string" className="add-todo-input-description" onChange={e => setDescription(e.target.value)} value={description} />
-						<div className="button" onClick={handleAddTodo}>Create Task</div>
-					</div>
-				</div>
-			) : ''}
-    </div>
+		    <div className="addPopup" onClick={handleOpen}>+</div>
+				 <Modal
+				 open={open}
+				 onClose={handleClose}
+				 aria-labelledby="modal-modal-title"
+				 aria-describedby="modal-modal-description"
+			   >
+				 <Box sx={ModalStyle}>
+				   <Typography id="modal-modal-title" variant="h6" component="h2" sx={{color: 'black'}}>
+					 Add a new REMINDER
+				   </Typography>
+				   <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+				   <Input
+				    type="text"
+					onChange={e => setNewTodo(e.target.value)}
+					value={newTodo} 
+					color='primary'
+					placeholder='Reminder title'/>
+				   </Typography>
+				   <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+				   <Input
+				    type="date"
+					onChange={e => setDueDate(e.target.value)} 
+					value={dueDate}
+					color='primary'
+					placeholder='Due date dd.mm.yyyy'/>
+				   </Typography>
+				   <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+				   <Input
+				    type="string"
+					multiline
+					onChange={e => setDescription(e.target.value)} 
+					value={description}
+					color='primary'
+					placeholder='Description'/>
+				   </Typography> <br />
+				   <Typography sx={{textAlign:'right'}}>
+				   <Button
+				   sx={{marginRight:'6px'}}
+				   onClick={handleClose}
+					color='primary'
+					variant='outlined'>Cancel
+					</Button> 
+				   <Button
+				    onClick={handleAddTodo}
+					color='primary'
+					variant='contained'>Create Task
+					</Button>
+				</Typography>
+				 </Box>
+			   </Modal>
+			</div>
   )
 }
 
