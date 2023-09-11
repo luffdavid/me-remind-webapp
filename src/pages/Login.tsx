2b3a2d4c11928192554db4f4cad322d7673cb932
luffdavid <luffd@mediamarktsaturn.de>
2023-09-11 14:20:30 +0200

    login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,59 +9,57 @@ import {
   MDBCheckbox
 }
 from 'mdb-react-ui-kit';
-import { LoginImage }  from '../assets/LoginImage.jpg'
+import  LoginImage from '../assets/LoginImage.jpg'
+import { Button, TextField } from '@mui/material';
+
 
 function Login() {
   return (
     <MDBContainer fluid className="p-3 my-5">
-      <span style={{color:'black', textAlign:'center'}}>LOGIN</span>
+      <h1 style={{color:'black', textAlign:'center'}}>LOGIN</h1>
       <MDBRow>
 
         <MDBCol col='10' md='6'>
-          <img src={LoginImage} className="img-fluid" alt="Phone image" />
+          <img src={LoginImage} className="img-fluid"  alt="Phone image" />
         </MDBCol>
         <br />
         <MDBCol col='4' md='6'>
-
-
-          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
-          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
-
-
+          {/* <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/> */}
+          <TextField
+         variant="outlined"
+         margin="normal"
+         required
+         fullWidth
+         label="Email"
+         type="email"
+/>
+<TextField
+             variant="outlined"
+             margin="normal"
+             required
+             fullWidth
+             label="Password"
+             type="password"
+/>
           <div className="d-flex justify-content-between mx-4 mb-4">
             <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
             <a href="!#">Forgot password?</a>
           </div>
 
-          <MDBBtn className="mb-4 w-100" size="lg">Sign in</MDBBtn>
-
-          <div className="divider d-flex align-items-center my-4">
-            <p className="text-center fw-bold mx-3 mb-0">OR</p>
-          </div>
-
-          <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#3b5998'}}>
-            <MDBIcon fab icon="facebook-f" className="mx-2"/>
-            Continue with facebook
-          </MDBBtn>
-
-
-
-
-
-
-
-
-
-
-          <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#55acee'}}>
-            <MDBIcon fab icon="twitter" className="mx-2"/>
-            Continue with twitter
-          </MDBBtn>
-
+          {/* <MDBBtn className="mb-4 w-100" size="lg">Sign in</MDBBtn> */}
+          <Button
+             type="submit"
+             fullWidth
+             variant="contained"
+             color="primary"
+             style={{ margin: '24px 0 16px' }}
+           >
+             Login
+             {/* {t("login", {ns: ['auth']})} */}
+           </Button>
         </MDBCol>
-
       </MDBRow>
-
     </MDBContainer>
   );
 }
